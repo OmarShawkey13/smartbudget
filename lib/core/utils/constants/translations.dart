3682426c8713ba76540780a7666b45960af18c97
fady41 <fadymakram141618@gmail.com>
2026-04-25 16:20:50 +0300

    update project

--- lib/core/utils/constants/translations.dart
+++
@@ -1,8 +0,0 @@
-class TranslationModel {
-  final Map<String, String> data;
-
-  TranslationModel.fromJson(Map<String, dynamic> json)
-    : data = json.map((key, value) => MapEntry(key, value.toString()));
-
-  String get(String key) => data[key] ?? key;
-}
